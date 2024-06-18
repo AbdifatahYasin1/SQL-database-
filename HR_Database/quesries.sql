3e9f4468a500470953f85e88d49b5e33dbca39a3
AbdifatahYasin1 <iskatag123@gmail.com>
2024-06-18 17:42:21 +0300

    addeed

---
+++ HR_Database/quesries.sql
@@ -0,0 +1,10 @@
+select  first_name  from Employees where id =1;
+select * from Employees
+
+select * from Employees where phone_number = '0798654321';
+select * from Employees where manager_id = 1;
+select * from Employees where not first_name = 'Ismaaciil';
+select * from Employees where id > 1;
+select * from Employees where id > 1 and id < 10;
+select * from Employees where id > 1 and id < 10 and role_id = 1;
+
